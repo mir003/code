71bf865c5e5fb4b5783266e515e6c67f6d3b4986
Mir Noshin Jahan <56441623+mir003@users.noreply.github.com>
2021-07-10 23:42:40 +0600

    cpp

--- a^b^c.cpp
+++ a^b^c.cpp
@@ -1,15 +1,7 @@
-<snippet>
-	<content><![CDATA[
-///fermat's little theorem -> a^(b^c) % MOD 
+///fermat's little theorem -> a^(b^c) % MOD
 /// where MOD is a prime number
-ll power2(ll a, ll b, ll c)
-{
+ll power2(ll a, ll b, ll c) {
     ll x = bigmod(b, c, MOD-1);
     return bigmod(a, x, MOD);
 }
-]]></content>
-	<!-- Optional: Set a tabTrigger to define how to trigger the snippet -->
-	<tabTrigger>a^b^c</tabTrigger>
-	<!-- Optional: Set a scope to limit where the snippet will trigger -->
-	<!-- <scope>source.python</scope> -->
-</snippet>
+
