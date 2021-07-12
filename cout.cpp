86b0ca50d46e981b36435c2493838d34cff6cb46
Mir Noshin Jahan <56441623+mir003@users.noreply.github.com>
2021-07-12 11:51:38 +0600

    cpp

--- cout.cpp
+++
@@ -1,9 +0,0 @@
-<snippet>
-	<content><![CDATA[
-cout<<res<<endl;
-]]></content>
-	<!-- Optional: Set a tabTrigger to define how to trigger the snippet -->
-	<tabTrigger>cout</tabTrigger>
-	<!-- Optional: Set a scope to limit where the snippet will trigger -->
-	<!-- <scope>source.python</scope> -->
-</snippet>
