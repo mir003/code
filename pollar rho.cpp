5d1cacffb5e2d3138895962de32ccad4f8d5e31a
Mir Noshin Jahan <56441623+mir003@users.noreply.github.com>
2021-08-02 21:05:15 +0600

    update

--- pollar rho.cpp
+++ pollar rho.cpp
@@ -1,3 +1,34 @@
+/*Only those who endure patiently will be given their reward without limit.*/
+#include<bits/stdc++.h>
+#define ll long long///never change it to int -_-
+#define pb push_back
+#define MAX 2e18
+#define MIN -2e18
+#define MAXi 1e9
+#define MINi -1e9
+#define MOD 1000000007
+///#define mod2 20071027
+//#define MOD 998244353
+#define base 139
+///#define MOD 1000074259
+#define base2 31
+#define memz(a) memset(a, 0, sizeof(a))
+#define memn(a) memset(a, -1, sizeof(a))
+#define in1(a) scanf("%lld", &a)
+#define in2(a, b) scanf("%lld%lld", &a, &b)
+#define in1i(a) scanf("%d", &a)
+#define in2i(a, b) scanf("%d%d", &a, &b)
+#define TC(c) printf("Case #%lld: ", ++c)
+#define out(x) cout << #x << " -> " << x << endl;
+#define pii pair<ll, ll>
+#define piii pair<ll, pii>
+#define pi4 pair<pii, pii>
+#define endl "\n"
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define FILE   freopen("input.txt", "r", stdin);  freopen("out.txt", "w", stdout);
+#define coutd cout<<fixed<<setprecision(10)//coutd<<res<<endl;
+using namespace std;
+const ll N = 300010, M = 1005;
 #define MAXL (50000>>5)+1
 #define GET(x) (mark[x>>5]>>(x&31)&1)
 #define SET(x) (mark[x>>5] |= 1<<(x&31))
@@ -105,6 +136,7 @@ void factorize(int n, vector<ll> &f) {
     if (n != 1) f.push_back(n);
 }
 void llfactorize(ll n, vector<ll> &f) {
+    out(n)
     if (n == 1)
         return ;
     if (n < 1e+9) {
@@ -116,8 +148,9 @@ void llfactorize(ll n, vector<ll> &f) {
         return ;
     }
     ll d = n;
-    for (int i = 2; d == n; i++)
+    for (int i = 2; d == n; i++){
         d = pollard_rho(n, i);
+    }
     llfactorize(d, f);
     llfactorize(n / d, f);
 }
--- rmq-o1.cpp
+++ rmq-o1.cpp
@@ -1,16 +1,17 @@
 ///sparse table for rmq O(1)**********
-ll n, a[N], st[N][20], st2[N][20], lg[N];
+ll n, a[N], st[N][20], lg[N];
 void b() {
     for(ll i=0; i<n; i++) {
         st[i][0]=a[i];
     }
     for(ll j=1; (1LL<<j)<=n; j++) {
         for(ll i=0; i+(1LL<<j)-1<n; i++) {
-            st[i][j]=st[i][j-1]<st[i+(1LL<<(j-1))][j-1]?st[i][j-1]:st[i+(1LL<<(j-1))][j-1];
+            st[i][j]=min(st[i][j-1],st[i+(1LL<<(j-1))][j-1]);
         }
     }
 }
 ll q(ll l, ll r) {
+    if(r>l) return MAX;//invalid for min=MAX, invalid for max=MIN, etc etc
     ll sz= r-l+1;
     ll k=lg[sz];
     ll x= st[l][k];
@@ -18,30 +19,10 @@ ll q(ll l, ll r) {
     if(sz>0) {
         k=lg[sz]+1;
         ll y= r-(1LL<<k)+1;
-        return x<st[y][k]?x:st[y][k];
+        return min(x,st[y][k]);
     } else return x;
 }
-void b2() {
-    for(ll i=0; i<n; i++) {
-        st2[i][0]=a[i];
-    }
-    for(ll j=1; (1LL<<j)<=n; j++) {
-        for(ll i=0; i+(1LL<<j)-1<n; i++) {
-            st2[i][j]=st2[i][j-1]>st2[i+(1LL<<(j-1))][j-1]?st2[i][j-1]:st2[i+(1LL<<(j-1))][j-1];
-        }
-    }
-}
-ll q2(ll l, ll r) {
-    ll sz= r-l+1;
-    ll k=lg[sz];///log[sz]
-    ll x= st2[l][k];
-    sz-=(1LL<<k);
-    if(sz>0) {
-        k=lg[sz]+1;
-        ll y= r-(1LL<<k)+1;
-        return x>st2[y][k]?x:st2[y][k];
-    } else return x;
-}
+
 int main() {
     lg[1] = 0;
     for (int i = 2; i <= N-5; i++)
@@ -51,10 +32,8 @@ int main() {
         in1(a[i]);
     ll l, r;
     b();
-    b2();
     while(cin>>l>>r) {
         cout<<l<<" "<<r<<" res "<<q(l,r)<<endl;
-        cout<<l<<" "<<r<<" res "<<q2(l,r)<<endl;
     }
     return 0;
 }
